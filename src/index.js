7ebe6289d85f63ee53fe5d5b40ce829bbf4d3ef8
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-06-17 01:51:59 -0600

    Create homepage layout and styling, images, buttons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import './style.css';
 
 function component() {
-    const content = document.getElementById('content');
+    const content = document.querySelector('.content');
+
+    const contentImg = document.createElement('div');
+    contentImg.classList.add('content-background');
+    content.appendChild(contentImg);
+
+    const h1Wrapper = document.createElement('div');
+    h1Wrapper.classList.add('h1-wrapper');
+    contentImg.appendChild(h1Wrapper);
 
     const nameTitle = document.createElement('h1');
-    nameTitle.classList.add('name-title');
+    nameTitle.classList.add('title');
     nameTitle.textContent = `fÜdhaÜs NYC`;
-    content.appendChild(nameTitle);
+    h1Wrapper.appendChild(nameTitle);
 
+    const introTxtBackground = document.createElement('div');
+    introTxtBackground.classList.add('intro-text-background');
+    contentImg.appendChild(introTxtBackground)
+    
     const introTxt = document.createElement('p');
     introTxt.classList.add('intro-txt');
     introTxt.textContent = `fÜdhaÜs NYC is the newest three Michelin-starred top tier exclusive dining experience in the heart of Manhattan. Nestled dauntingly high atop One Vanderbilt in Midtown, it is the newest creation of celebrity chef Drake Knöpfnenfüdsensön. Reveal your hidden pallet and engage all your senses with "The Knopf"'s culinary mouth-child... fÜdhaÜs New York.`;
-    content.appendChild(introTxt);
+    introTxtBackground.appendChild(introTxt);
 }
 
 document.body.appendChild(component());
