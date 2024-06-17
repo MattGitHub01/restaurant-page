77d44f0a9b379e510dd6b643977ed5d9d49d087a
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-06-17 03:19:08 -0600

    Create changing DOM page code in button event listeners

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './index.css';
+
 import { homeBtn, menuBtn, aboutBtn } from './switch';
 
 homeBtn()
@@ -21,15 +22,6 @@ function component() {
     nameTitle.classList.add('title');
     nameTitle.textContent = `fÜdhaÜs NYC`;
     h1Wrapper.appendChild(nameTitle);
-
-    const introTxtBackground = document.createElement('div');
-    introTxtBackground.classList.add('intro-text-background');
-    contentImg.appendChild(introTxtBackground)
-    
-    const introTxt = document.createElement('p');
-    introTxt.classList.add('intro-txt');
-    introTxt.textContent = `fÜdhaÜs NYC is the newest three Michelin-starred top tier exclusive dining experience in the heart of Manhattan. Nestled dauntingly high atop One Vanderbilt in Midtown, it is the newest creation of celebrity chef Drake Knöpfnenfüdsensön. Reveal your hidden pallet and engage all your senses with "The Knopf"'s culinary mouth-child... fÜdhaÜs New York.`;
-    introTxtBackground.appendChild(introTxt);
 }
 
 document.body.appendChild(component());
--- src/switch.js
+++ src/switch.js
@@ -1,7 +1,29 @@
 export function homeBtn() {
-    const homeBtn = document.querySelector('.home-btn');
-    homeBtn.addEventListener('click', () => {
-        alert('home!');
+const homeBtn = document.querySelector('.home-btn');
+homeBtn.addEventListener('click', () => {
+    const content = document.querySelector('.content');
+
+    const contentImg = document.createElement('div');
+    contentImg.classList.add('home-content-background');
+    content.appendChild(contentImg);
+
+    const h1Wrapper = document.createElement('div');
+    h1Wrapper.classList.add('home-h1-wrapper');
+    contentImg.appendChild(h1Wrapper);
+
+    const nameTitle = document.createElement('h1');
+    nameTitle.classList.add('home-title');
+    nameTitle.textContent = `fÜdhaÜs NYC`;
+    h1Wrapper.appendChild(nameTitle);
+
+    const introTxtBackground = document.createElement('div');
+    introTxtBackground.classList.add('home-intro-text-background');
+    contentImg.appendChild(introTxtBackground)
+    
+    const introTxt = document.createElement('p');
+    introTxt.classList.add('home-intro-txt');
+    introTxt.textContent = `fÜdhaÜs NYC is the newest three Michelin-starred top tier exclusive dining experience in the heart of Manhattan. Nestled dauntingly high atop One Vanderbilt in Midtown, it is the newest creation of celebrity chef Drake Knöpfnenfüdsensön. Reveal your hidden pallet and engage all your senses with "The Knopf"'s culinary mouth-child... fÜdhaÜs New York.`;
+    introTxtBackground.appendChild(introTxt);
     });
 }
 
