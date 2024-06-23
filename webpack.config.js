fcea35dacc57c5615d403ee648fc57fa2381eeb0
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-06-23 13:25:03 -0600

    Add webpage icon image via webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html',
+            favicon: "./src/images/icon.svg",
             inject: 'head',
             scriptLoading: 'defer'
         })
